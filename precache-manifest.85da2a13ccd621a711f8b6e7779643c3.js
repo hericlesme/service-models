7130779120ff5f20e5ee6f52c0b78b84a08d9883
Héricles <hericles.me@gmail.com>
2020-06-27 15:31:43 -0300

    Updates

---
+++ precache-manifest.85da2a13ccd621a711f8b6e7779643c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5cf1347d75eb2a4010265e7012c8b43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cb035ba2bbcc9f6c045",
+    "url": "/static/css/main.bd0c8ec7.chunk.css"
+  },
+  {
+    "revision": "3d5572c689047eb0b9cb",
+    "url": "/static/js/2.736c1724.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.736c1724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb035ba2bbcc9f6c045",
+    "url": "/static/js/main.48f5e5a8.chunk.js"
+  },
+  {
+    "revision": "6fd48943783c7121ab15",
+    "url": "/static/js/runtime-main.a3197287.js"
+  }
+]);
